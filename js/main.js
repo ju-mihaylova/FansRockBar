f59172b401722218ef3d9dca42f1d1c00a241d43
Jasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-04-28 22:24:34 +0300

    Placed website styles and code in external files

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+// Automatic Slideshow - change image every 4 seconds
+let index = 0;
+carousel();
+
+function carousel() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i+=1) {
+        slides[i].style.display = "none";
+    }
+    index+=1;
+    if (index > slides.length) {index = 1}
+    slides[index-1].style.display = "block";
+    setTimeout(carousel, 4000);
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleMenu() {
+    let navSmall = document.getElementById("nav-sm-scr");
+    if (navSmall.className.indexOf("show") === -1) {
+        navSmall.className += " show";
+    } else {
+        navSmall.className = navSmall.className.replace(" show", "");
+    }
+}
+
+const navLinks = document.querySelectorAll('#nav-sm-scr a');
+for (const navLink of navLinks) {
+    navLink.addEventListener('click', toggleMenu);
+}
+
+const barsMenu = document.querySelector('#bars-menu');
+barsMenu.addEventListener('click', e => e.preventDefault());
+barsMenu.addEventListener('click', toggleMenu);
+
+// When the user clicks anywhere outside of the modal, close it
+let modal = document.getElementById('ticket-modal');
+window.onclick = function(e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+};
+const navSearch = document.getElementById('nav-search');
+navSearch.addEventListener('click', function (e) {
+   e.preventDefault();
+});
